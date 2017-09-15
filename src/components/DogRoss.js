779f5303ff5fe87bf12a310cf1625607c109e6b4
jordanstoll23 <jdogstoll@yahoo.com>
2017-09-15 12:06:00 -0700

    ss

--- src/components/DogRoss.js
+++
@@ -1,9 +0,0 @@
-import React, { component } from 'react';
-
-class DogRoss extends Component {
-    render() {
-        return <div> Jordan </div>;
-    }
-}
-
-export default Jordan;
--- src/components/Pizza.js
+++
@@ -1,9 +0,0 @@
-import React , {component} from 'react';
-
-class Pizza extends Component{
-    render(){
-        return <div>Pizza</div>;
-    }
-}
-
-export default Pizza;
