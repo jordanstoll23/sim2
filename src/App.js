0571d7e487ce3238d16f10a8bb91af7d680c2e42
jordanstoll23 <jdogstoll@yahoo.com>
2017-09-15 14:23:02 -0700

    added basic database funcionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import Dash from './components/dash'
+import Dash from './components/dash';
+import Wizard from './components/wizard';
 
 
 class App extends Component {
@@ -13,8 +14,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/dash" component={Dash} />
-            {/* <Route path="/bin/:id" component={Inventory} />
-            <Route path="/create/:id" component={Create} /> */} */}
+            <Route path="/wizard" component={Wizard} />
+            {/* <Route path="/create/:id" component={Create} /> */}
           </Switch>
         </HashRouter>
       </div>
